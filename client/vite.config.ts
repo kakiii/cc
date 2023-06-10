749a3903193597dbd7b4939dd1e2bc3fa0f4c349
kakiii <kakiatms@outlook.com>
2023-06-10 21:54:30 +0200

    update proxy rules

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,7 +11,27 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
         ws: false,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
+          });
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
+          });
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
+          });
+        },
+      },
+      "/chatgpt": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        ws: false,
         configure: (proxy, _options) => {
           proxy.on("error", (err, _req, _res) => {
             console.log("proxy error", err);
@@ -29,5 +49,7 @@ export default defineConfig({
         },
       },
     },
+    
+    
   },
 });
