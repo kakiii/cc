1f43de04a9c7c77adeef15f3b6df686f9ec09752
kakiii <kakiatms@outlook.com>
2023-06-10 20:30:43 +0200

    feat: gpt helloworld done

--- server/src/routes/chatgpt.js
+++ server/src/routes/chatgpt.js
@@ -1,24 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const openai = require('openai');
+const { Configuration, OpenAIApi } = require('openai');
+require('dotenv').config();
 
-openai.apiKey = process.env.OPENAI_SECRET_KEY;
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 router.post('/hello', async (req, res) => {
     const inputMessage = req.body.message;
     try {
-        const gptResponse = await openai.Completion.create({
-            engine: "gpt-3.5-turbo",
-            prompt: inputMessage,
-            max_tokens: 100
+        const gptResponse = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            max_tokens: 100,
+            messages: [
+                {
+                    role:"user",
+                    content: inputMessage,
+                }]
         });
 
-        const outputMessage = gptResponse.data.choices[0].text;
+        const outputMessage = gptResponse.data.choices[0].message;
         res.json({ message: outputMessage });
     } catch (err) {
         console.error(err);
+        if (err.response) {
+            console.log(err.response.status);
+            console.log(err.response.data);
+        } else {
+            console.log(err.message);
+        }
         res.status(500).json({ error: "An error occurred when trying to communicate with GPT-3" });
     }
+});
+
+router.get('/hello', (req, res) => {
+    res.send('Hello World!');
 });
 
 module.exports = router;
